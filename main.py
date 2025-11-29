1872852fe58f91d6f46f12712a3dd1444efe0d11
yadrien <adrien.yapoudjian@ynov.com>
2025-11-29 12:41:46 +0100

    ajout des command que l'utilisateur pourras faire

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Node:
         self.next = None
 
 class CommandHistory:
-    def __init__(self):
+    def __init__(self):     
         self.head = None
 
     def add_command(self, command: str):
@@ -31,6 +31,8 @@ class CommandHistory:
     def clear(self):
         self.head = None
 
+histo_user: dict[int, CommandHistory] = {}     
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user}')
@@ -39,8 +41,36 @@ class Client(discord.Client):
         if message.author == self.user:
             return
         
-        if message.content.startswith('/help'):
-            await message.channel.send('Hello ')
+        content = message.content.strip()
+        user_id = message.author.id
+
+        if user_id not in histo_user:
+            histo_user[user_id] = CommandHistory()
+        history = histo_user[user_id]
+
+        if content.startswith("/"):
+            history.add_command(content)
+        
+        if content.startswith("/last"):
+            last = history.get_last()
+            if last is None:
+                await message.channel.send("Tu n'as encore envoyé aucune commande.")
+            else:
+                await message.channel.send(f"Dernière commande enregistrée : `{last}`")
+
+        elif content.startswith("/history"):
+            all_commands = history.get_all()
+            if not all_commands:
+                await message.channel.send("Ton historique est vide.")
+            else:
+                text = "\n".join(all_commands)
+                await message.channel.send(f"Historique des commandes :\n{text}")
+
+        elif content.startswith("/clear_history"):
+            history.clear()
+            await message.channel.send("Historique Vidé.")
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
